250beadb7c7b773909571e933bacd71fc4a637d2
Jogalaxmi Rath <jogalaxmirath@gmail.com>
2024-02-23 08:41:06 +0530

    Adding our Serverless API

---
+++ list.js
@@ -0,0 +1,27 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": "123",
+    },
+  };
+
+  try {
+    const result = await dynamoDb.query(params).promise();
+
+    // Return the matching list of items in the response body
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message }),
+    };
+  }
+});
